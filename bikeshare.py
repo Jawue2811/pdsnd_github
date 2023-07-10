53a65983d41d88a2bb38577f3a6fda004b1fc3ef
Jasper Wue <jasper.w@live.de>
2023-07-10 14:23:14 +0200

    Docs: Changes documentation in .py file for day filter

--- bikeshare.py
+++ bikeshare.py
@@ -76,7 +76,7 @@ def load_data(city, month, day):
         # filter by month to create the new dataframe
         df = df[df['month'] == month]
    
-    # filter by day of week if applicable
+    # filter by day of week if applicable, if not applicable the function will end
     if day != 'all':
         # filter by day of week to create the new dataframe
         df = df[df['day_of_week'] == day.title()]
